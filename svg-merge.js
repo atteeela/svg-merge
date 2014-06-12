24870eb944e29ec92adea01995c563a7a3ef7446
P.J. Onori <pj@somerandomdude.com>
2014-06-11 20:26:09 -0700

    Minor tweaks

--- svg-merge.js
+++ svg-merge.js
@@ -10,6 +10,9 @@ module.exports = function (opts, done) {
   var inputDir = opts.inputDir;
   var outputDir = opts.outputDir;
   var outputFile = opts.outputFile;
+  var iconName;
+
+  var dimensions = [];
 
   // Use optional outputFile name or set it equal to the output dir
   // plus a '-out' suffix. So foo/arrow would produce bar/arrow/arrow-out.svg
@@ -40,13 +43,24 @@ module.exports = function (opts, done) {
       var svgXml = fs.readFileSync(file, 'utf8');
       var svg = easy.parse(svgXml);
 
+      //record the dimensions of the svg
+      var dim =Array(
+        Number(parseFloat(svg.$width, 10)),
+        Number(parseFloat(svg.$height, 10))
+      )
+      dimensions.push( dim );
+      
+
       // strip out opening and closing svg tags
       // returning just a string with the internals
+
+      iconName = svg.$.toString().match(/ data-icon="([^"]*)"/)[1];
+
       svg = svg.$.toString()
         .replace(/<svg[^<]*/, '')
         .replace(/<\/svg>$/, '');
 
-      var group = easy.parse(['<g>', svg, '</g>'].join(''));
+      var group = easy.parse(['<g data-width="'+ String(dim[0]) +'" data-height="'+ String(dim[1]) +'">', svg, '</g>'].join(''));
 
       stack.push({
         group: group.$.toString()
@@ -57,11 +71,27 @@ module.exports = function (opts, done) {
 
   function merge(stack, callback) {
     var svg = [];
-    svg.push('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">');
+
+    //find largest bounding box
+    var bBox = dimensions.reduce(function(max, arr) {
+      return max >= arr[0] ? max : arr[0];
+    }, -Infinity);
+
+    var areas = [];
+    for ( var i = 0; i < dimensions.length; i++) {
+      areas.push(dimensions[i][0]*dimensions[i][1]);
+    }
+
+
+    var ndx = areas.indexOf(Math.max.apply(Math, areas));
+
+    svg.push('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" data-icon="' + iconName + '" width="' + Math.ceil(dimensions[ndx][0]) + 'px" height="' + Math.ceil(dimensions[ndx][1]) + 'px" viewBox="0 0 ' + Math.ceil(dimensions[ndx][0]) + ' ' + Math.ceil(dimensions[ndx][1]) + '">');
     // need to make same ids from different files unique
     stack.forEach(function(icon) {
       svg.push(icon.group);
     });
+
+
     
     svg.push('</svg>');
 
